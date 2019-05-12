b794dd9f30ea16387f0da4ad53c25298e5e8b14e
Alan Mac Kenna <alan.mackenna@serveit.ie>
2019-05-12 20:44:16 +0100

    added privacy policy & cookie policy health checks

---
+++ src/Our.Umbraco.HealthChecks/Checks/Privacy/CookiePolicyCheck.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using Umbraco.Core.Services;
+using Umbraco.Web.HealthCheck;
+using Umbraco.Web.HealthCheck.Checks.Config;
+
+namespace Our.Umbraco.HealthChecks.Checks.Privacy
+{
+
+    [HealthCheck("4A616B92-AC7C-4CA0-9F77-08E8543AEC8E", "Cookie Policy Check - (from Our.Umbraco.HealthChecks)",
+        Description = "Check to ensure a Cookie Policy is selected",
+        Group = "Privacy")]
+    public class CookiePolicyCheck : AbstractConfigCheck
+    {
+        protected readonly ILocalizedTextService TextService;
+
+        public CookiePolicyCheck(HealthCheckContext healthCheckContext) : base(healthCheckContext)
+        {
+            TextService = healthCheckContext.ApplicationContext.Services.TextService;
+        }
+
+        public override string FilePath => "~/Web.config";
+
+        public override string XPath => "/configuration/appSettings/add[@key='Our.Umbraco.HealthChecks.Privacy.CookiePolicyUDI']/@value";
+
+        public override ValueComparisonType ValueComparisonType => ValueComparisonType.ShouldNotEqual;
+
+        public override ProvidedValuePropertyType ProvidedValuePropertyType => ProvidedValuePropertyType.ContentPicker;
+
+        public override IEnumerable<AcceptableConfiguration> Values => new List<AcceptableConfiguration>
+        {
+            new AcceptableConfiguration { IsRecommended = false, Value = string.Empty }
+        };
+
+        public override string CheckSuccessMessage => HealthCheckContext.ApplicationContext.Services.TextService.Localize("Our.Umbraco.HealthChecks/cookiePolicyConfigSuccess");
+
+        public override string CheckErrorMessage => HealthCheckContext.ApplicationContext.Services.TextService.Localize("Our.Umbraco.HealthChecks/cookiePolicyError");
+
+        public override string RectifySuccessMessage => HealthCheckContext.ApplicationContext.Services.TextService.Localize("Our.Umbraco.HealthChecks/cookiePolicyRectifySuccess");
+    }
+
+}
---
+++ src/Our.Umbraco.HealthChecks/Checks/Privacy/PrivacyPolicyCheck.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using Umbraco.Core.Services;
+using Umbraco.Web.HealthCheck;
+using Umbraco.Web.HealthCheck.Checks.Config;
+
+namespace Our.Umbraco.HealthChecks.Checks.Privacy
+{
+
+    [HealthCheck("4E7B05BE-F77D-4B8A-8E77-F0FB96E0BDF8", "Privacy Policy Check - (from Our.Umbraco.HealthChecks)",
+        Description = "Check to ensure a Privacy Policy is selected.",
+        Group = "Privacy")]
+    public class PrivacyPolicyCheck : AbstractConfigCheck
+    {
+        protected readonly ILocalizedTextService TextService;
+
+        public PrivacyPolicyCheck(HealthCheckContext healthCheckContext) : base(healthCheckContext)
+        {
+            TextService = healthCheckContext.ApplicationContext.Services.TextService;
+        }
+
+        public override string FilePath => "~/Web.config";
+
+        public override string XPath => "/configuration/appSettings/add[@key='Our.Umbraco.HealthChecks.Privacy.PrivacyPolicyUDI']/@value";
+
+        public override ValueComparisonType ValueComparisonType => ValueComparisonType.ShouldNotEqual;
+
+        public override ProvidedValuePropertyType ProvidedValuePropertyType => ProvidedValuePropertyType.ContentPicker;
+
+        public override IEnumerable<AcceptableConfiguration> Values => new List<AcceptableConfiguration>
+        {
+            new AcceptableConfiguration { IsRecommended = false, Value = string.Empty }
+        };
+
+
+        public override string CheckSuccessMessage => HealthCheckContext.ApplicationContext.Services.TextService.Localize("Our.Umbraco.HealthChecks/privacyPolicyConfigSuccess");
+
+        public override string CheckErrorMessage => HealthCheckContext.ApplicationContext.Services.TextService.Localize("Our.Umbraco.HealthChecks/privacyPolicyError");
+
+        public override string RectifySuccessMessage => HealthCheckContext.ApplicationContext.Services.TextService.Localize("Our.Umbraco.HealthChecks/privacyPolicyRectifySuccess");
+    }
+
+}
